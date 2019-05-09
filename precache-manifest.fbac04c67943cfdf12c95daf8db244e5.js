87790a68c10cbbd1082268c25c1aa7fade749fc0
Allude <radulescualex@gmail.com>
2019-05-08 22:22:14 -0400

    Updates

---
+++ precache-manifest.fbac04c67943cfdf12c95daf8db244e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6370eee75d298d3e90d5270dcdd19e24",
+    "url": "/mortgage-calculator/index.html"
+  },
+  {
+    "revision": "69df0528686226d108f9",
+    "url": "/mortgage-calculator/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "8e7a299250b44eeb1141",
+    "url": "/mortgage-calculator/static/css/main.1c52867a.chunk.css"
+  },
+  {
+    "revision": "69df0528686226d108f9",
+    "url": "/mortgage-calculator/static/js/2.6928a4a2.chunk.js"
+  },
+  {
+    "revision": "8e7a299250b44eeb1141",
+    "url": "/mortgage-calculator/static/js/main.2f456439.chunk.js"
+  },
+  {
+    "revision": "073f7b3802bbf451000f",
+    "url": "/mortgage-calculator/static/js/runtime~main.05b587da.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mortgage-calculator/precache-manifest.3a979a95370fd6b9e02ffa14efbf0b23.js"
+  "/mortgage-calculator/precache-manifest.fbac04c67943cfdf12c95daf8db244e5.js"
 );
 
 self.addEventListener('message', (event) => {
